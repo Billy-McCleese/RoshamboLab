a581f75e6f603a77bfa11ae007bd451cab046eb8
Billy-McCleese <billymccleese@gmail.com>
2023-02-20 16:50:20 -0500

    commit for the Roshambo lab

---
+++ RoshamboLab/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RoshamboLab
+{
+    public abstract class Player
+    {
+        public string PlayerName { get; set; }
+        public string RoshamboUserValue { get; protected set; }
+
+        public abstract RoshamboEnum GenerateRoshambo();
+    }
+
+    public class RockPlayer : Player
+    {
+        public RockPlayer(string name)
+        {
+            PlayerName = name;
+        }
+        public override RoshamboEnum GenerateRoshambo()
+        {
+            return RoshamboEnum.rock;
+        }
+    }
+
+    public class RandomPlayer : Player
+    {
+        private static readonly Random _random = new Random();
+
+        public RandomPlayer(string name)
+        {
+            PlayerName = name;
+        }
+
+        public override RoshamboEnum GenerateRoshambo()
+        {
+            Array values = Enum.GetValues(typeof(RoshamboEnum));
+            return (RoshamboEnum)values.GetValue(_random.Next(values.Length));
+        }
+    }
+    public class HumanPlayer : Player
+    {
+        public HumanPlayer(string name)
+        {
+            PlayerName = name;
+        }
+
+        public override RoshamboEnum GenerateRoshambo()
+        {
+            while (true)
+            {
+                Console.WriteLine($"{PlayerName}, please choose Rock (R), Paper (P), or Scissors (S):");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "r")
+                    return RoshamboEnum.rock;
+                else if (input == "p")
+                    return RoshamboEnum.paper;
+                else if (input == "s")
+                    return RoshamboEnum.scissors;
+
+                Console.WriteLine("This is not a valid input, please try again.");
+            }
+        }
+    }
+}
+
+
---
+++ RoshamboLab/Program.cs
@@ -0,0 +1,61 @@
+﻿using RoshamboLab;
+using System.ComponentModel;
+
+Console.WriteLine("Welcome to Roshambo!");
+
+// Create a HumanPlayer using the human player class
+Console.Write("Enter your name: ");
+string playerName = Console.ReadLine();
+HumanPlayer humanPlayer = new HumanPlayer(playerName);
+
+Player Opponent = null;
+while (Opponent == null)
+{
+    Console.WriteLine("Who would you like to play against, RockPlayer(1) or RandomPlayer(2): (1/2)?");
+    string OpponentNum = Console.ReadLine();
+
+    if (OpponentNum == "1")
+    {
+        Opponent = new RockPlayer("RockPlayer");
+    }
+    else if (OpponentNum == "2")
+    {
+        Opponent = new RandomPlayer("RandomPlayer");
+    }
+    else Console.WriteLine("Invalid input, please try again");
+}
+
+
+while (true)
+{
+    // Get the players' choices by calling the inherrited classes
+    RoshamboEnum humanChoice = humanPlayer.GenerateRoshambo();
+    RoshamboEnum opponentChoice = Opponent.GenerateRoshambo();
+
+    Console.WriteLine($"{humanPlayer.PlayerName} chose {humanChoice}.");
+    Console.WriteLine($"{Opponent.PlayerName} chose {opponentChoice}.");
+
+    // Determine the winner using if loop
+    if (humanChoice == opponentChoice)
+    {
+        Console.WriteLine("It's a tie!");
+    }
+    else if (humanChoice == RoshamboEnum.rock && opponentChoice == RoshamboEnum.scissors ||
+             humanChoice == RoshamboEnum.paper && opponentChoice == RoshamboEnum.rock ||
+             humanChoice == RoshamboEnum.scissors && opponentChoice == RoshamboEnum.paper)
+    {
+        Console.WriteLine($"{playerName} wins!");
+    }
+    else
+    {
+        Console.WriteLine($"{Opponent.PlayerName} wins!");
+    }
+
+    // Ask the user if they want to play again
+    Console.WriteLine("Play again? (Y/N)");
+    string playAgain = Console.ReadLine().ToLower();
+    if (playAgain != "y")
+        break;
+}
+
+Console.WriteLine("Thanks for playing Roshambo!");
